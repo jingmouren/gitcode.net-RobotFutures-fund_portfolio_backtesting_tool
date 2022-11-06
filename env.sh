125cfa1d13b0e23991af08383b4bdbce8ca3816c
628739352ad51175a75aca32 <628739352ad51175a75aca32@devide>
2022-11-06 22:45:34 +0800

    环境配置脚本增加时区配置

--- env.sh
+++ env.sh
@@ -2,3 +2,5 @@
 pip config set global.index-url https://mirrors.aliyun.com/pypi/simple/ 
 # 安装中文字体
 cp fonts/simhei.ttf /usr/share/fonts/&&apt install -y ttf-mscorefonts-installer fontconfig&&mkfontscale&&fc-cache
+# 修改时区为东八区
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
